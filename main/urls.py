7229e15ea2e7959f8163d20ddbf58499817d9643
syifakaffa <syifa.kaffa@ui.ac.id>
2023-09-27 05:34:39 +0700

    fix urls.py, Tugas4 Selesai.

--- main/urls.py
+++ main/urls.py
@@ -7,7 +7,7 @@ app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
-    path('create-product', create_product, name='create_product'),
+    path('create-product/', create_product, name='create_product'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
     path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
