08ca2ea4eb33ee54452102a2f8595bd65c7da195
syifakaffa <syifa.kaffa@ui.ac.id>
2023-10-04 09:34:08 +0700

    add edit product

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from main.views import show_main, create_product, show_xml, show_json, show_json_by_id, show_xml_by_id
 from main.views import register, login_user, logout_user
-from main.views import add_one, dec_one, delete_product
+from main.views import add_one, dec_one, delete_product, edit_product
 
 app_name = 'main'
 
@@ -18,4 +18,5 @@ urlpatterns = [
     path('add-one/<int:product_id>/', add_one, name='add_one'),
     path('dec-one/<int:product_id>/', dec_one, name='dec_one'),
     path('delete/<int:product_id>/', delete_product, name='delete_product'),
+    path('edit-product/<int:id>', edit_product, name='edit_product'),
 ]
